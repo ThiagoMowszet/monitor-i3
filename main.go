5a93f4f2d12eabed279da4cc92fca7495f5ab19f
Thiago <thiagomowszet@gmail.com>
2024-03-28 23:44:42 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "os/exec"
+)
+
+func main() {
+    // Definir el comando y sus argumentos
+    cmd := exec.Command("xrandr", "--output", "HDMI-2", "--mode", "1920x1080", "--rate", "144")
+
+    // Ejecutar el comando y manejar errores
+    if err := cmd.Run(); err != nil {
+        fmt.Println("Error al ejecutar el comando:", err)
+        return
+    }
+
+}
+
