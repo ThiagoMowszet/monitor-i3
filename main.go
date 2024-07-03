6cb05e678fec8e6ece85d067a3dbc73cbcf2b59e
Thiago <thiagomowszet@gmail.com>
2024-07-02 21:02:33 -0300

    feat: update monitor to 1

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "os/exec"
+	"fmt"
+	"os/exec"
 )
 
 func main() {
-    // Definir el comando y sus argumentos
-    cmd := exec.Command("xrandr", "--output", "HDMI-2", "--mode", "1920x1080", "--rate", "144")
+	// Definir el comando y sus argumentos
+	cmd := exec.Command("xrandr", "--output", "HDMI-1", "--mode", "1920x1080", "--rate", "144")
 
-    // Ejecutar el comando y manejar errores
-    if err := cmd.Run(); err != nil {
-        fmt.Println("Error al ejecutar el comando:", err)
-        return
-    }
+	// Ejecutar el comando y manejar errores
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error al ejecutar el comando:", err)
+		return
+	}
 
 }
-
